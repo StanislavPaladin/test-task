25f5f8e71e76df9d4fb15b977cfcd4eb09735b3d
Stanislav Paladin <paladinstanis@gmail.com>
2021-04-11 19:47:47 +0300

    fixed some bugs

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 'use strict'
 document.addEventListener('DOMContentLoaded', function (event) {
-    const emailWrapper = document.querySelector('.survey__wrapper')
-    const email = document.querySelector('.survey__wrapper-email');
     const emailInput = document.querySelector('.email-input');
     const inputCounter = document.querySelector('.input-counter');
     const emailYour = document.querySelector('.email-your');
@@ -11,7 +9,6 @@ document.addEventListener('DOMContentLoaded', function (event) {
         if (emailInput.value[0] != '@') {
             emailInput.value = '@ |' + emailInput.value;
         }
-        // emailInput.value = emailInput.value.splice(1)
         emailInput.value = emailInput.value.split(' ').join('');
         const yourEmail = emailInput.value;
         let count = yourEmail.length -2;
@@ -27,12 +24,10 @@ document.addEventListener('DOMContentLoaded', function (event) {
 
     })
     emailInput.addEventListener('focus', (e) => {
-        // e.preventDefault();
         emailInput.value = emailInput.value.split(' ').join('');
         const yourEmail = emailInput.value;
         let count = yourEmail.length;
         emailInput.style.border = '1px solid green'
-        // inputCounter.classList.remove('hide');
             emailYour.innerHTML = `
             Ваш email: ${yourEmail.slice(2)} </div><i class="fa fa-info-circle" aria-hidden="true" contenteditable="false"></i>
             `;
@@ -44,9 +39,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
             
     })
     emailInput.addEventListener('focusout', (e) => {
-        // e.preventDefault()
         emailInput.style.border = '1px solid gray'
-        // inputCounter.classList.add('hide');
         inputCounter.textContent = ''
     })
 })
